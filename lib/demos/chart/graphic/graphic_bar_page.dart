fcc1db9be11c326d258c8ad4d8c99cac9961d390
iotjin <179608047@qq.com>
2023-04-13 11:34:44 +0800

    Update graphic（2.0.3）

--- lib/demos/chart/graphic/graphic_bar_page.dart
+++ lib/demos/chart/graphic/graphic_bar_page.dart
@@ -349,14 +349,14 @@ Widget _initBarChart5() {
 const _kBaseGroupPaddingHorizontal = 32.0;
 const _kMinBarSize = 4.0;
 
-/// Changes the position of elements while also updating their width to match
-/// the number of elements in a single band. Useful for bar charts when the
+/// Changes the position of marks while also updating their width to match
+/// the number of marks in a single band. Useful for bar charts when the
 /// width of the bars can be dynamic.
 @immutable
 class DodgeSizeModifier extends Modifier {
   @override
-  void modify(AttributesGroups groups, Map<String, ScaleConv<dynamic, num>> scales, AlgForm form, CoordConv coord,
-      Offset origin) {
+  AttributesGroups modify(AttributesGroups groups, Map<String, ScaleConv<dynamic, num>> scales, AlgForm form,
+      CoordConv coord, Offset origin) {
     final xField = form.first[0];
     final band = (scales[xField]! as DiscreteScaleConv).band;
 
@@ -378,19 +378,33 @@ class DodgeSizeModifier extends Modifier {
     // Negatively shift half of the total bias.
     var accumulated = -bias * (numGroups + 1) / 2;
 
+    final AttributesGroups rst = [];
     for (final group in groups) {
+      final groupRst = <Attributes>[];
       for (final attributes in group) {
         final oldPosition = attributes.position;
-        attributes.position = oldPosition
-            .map(
-              (point) => Offset(point.dx + accumulated + bias, point.dy),
-            )
-            .toList();
 
-        attributes.size = size;
+        groupRst.add(Attributes(
+          index: attributes.index,
+          tag: attributes.tag,
+          position: oldPosition
+              .map(
+                (point) => Offset(point.dx + accumulated + bias, point.dy),
+              )
+              .toList(),
+          shape: attributes.shape,
+          color: attributes.color,
+          gradient: attributes.gradient,
+          elevation: attributes.elevation,
+          label: attributes.label,
+          size: size,
+        ));
       }
+      rst.add(groupRst);
       accumulated += bias;
     }
+
+    return rst;
   }
 
   @override
